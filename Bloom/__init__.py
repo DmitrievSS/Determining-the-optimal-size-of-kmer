032c1045428d3fba48fa68af4a875bd76a6b5f4e
serg <serg@unnormal.(none)>
2014-04-09 03:36:54 +0400

    test

--- Bloom/__init__.py
+++ Bloom/__init__.py
@@ -26,21 +26,23 @@ def exist(bf, kmer, differentkmers, kstart, line):
 # @timing_val
 def getkmers(k):
     # input = open("Bloom/input", "r")
-    input = open("input", "r")
+    input = open("../../norm.fastq", "r")
     return input
 
 
 @timing_val
 def run(kstart, kend):
     differentkmers = [0 for _ in xrange(kstart, kend)]
-    bloomFilter = [BloomFilter(6000, 0.01) for _ in xrange(kstart, kend)]
+    bloomFilter = [BloomFilter(50000000, 0.01) for _ in xrange(kstart, kend)]
     # with open("Bloom/output", "w") as output:
     with open("output", "w") as output:
         strInput = getkmers(kstart)
         # done = 0
         for line in strInput:
-            line = line[:-1]
-            exist(bloomFilter, line[:kend], differentkmers, kstart, line)
+            if line[0] == '@':
+                line = strInput.next()
+                line = line[:-1]
+                exist(bloomFilter, line[:kend], differentkmers, kstart, line)
 
         map(lambda x, y: output.write(str(y + 1) + "  different kmers = " +
                                       str(x) + "\n"), differentkmers, xrange(kstart, kend))
