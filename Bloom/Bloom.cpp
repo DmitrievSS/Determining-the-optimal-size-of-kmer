47305b6ff104cccc69c6728a65916cf13a621538
serg <serg@unnormal.(none)>
2014-04-13 19:25:07 +0400

    c++ bloom

--- Bloom/Bloom.cpp
+++ Bloom/Bloom.cpp
@@ -1,5 +1,5 @@
 #define MAXKMER 40
-#define MINKMER 40
+#define MINKMER 20
 #include <vector>
 #include <string>
 #include <math.h>
@@ -22,7 +22,7 @@ private:
     vector< vector<int> > hashtable;
 public:
     int k;
-    bloom_filter(int n, int p){
+    bloom_filter(int n, double p){
         m = -n * log(p) / (log(2)*log(2));
         k = m / n * log(2);
         list = vector<bool>(m);
@@ -31,27 +31,32 @@ public:
 
         tmp = vector<int>(k);
         for (int i = 0; i < k; i++){
-            int x = i;
+            int x = (2*i) + 1;
             hashtable.push_back(vector<int>(MAXKMER));
-            hashtable[i].push_back(x + 1);
+            hashtable[i][0] = x;
             for (int j = 1; j < MAXKMER; j++){
-                hashtable[i].push_back(hashtable[i][j - 1]*(x + 1));
+                hashtable[i][j] = (hashtable[i][j - 1]*(x));
             }
         }
     }
     
     int hashfunc(int k, string str, int prev = -1, char c = '_'){
-        int x = k;
+        int x = 2*(k-1) + 1;
         int res = 0;
         if (prev == -1 && c == '_'){
             for (int i = 0; i < str.size(); i++){
-                res = (res + str[i] * hashtable[x - 1][i]); 
+                res = (res + str[i] * hashtable[k - 1][str.size() - i - 1]) ; 
             }
+            //cout <<str.size()<<"  "<< res << " full hash \n";
         }
-        if (prev != -1 && c == '_')
-            res = prev + str[str.size() - 1] * hashtable[x - 1][str.size() - 1];
-        if (prev != -1 && c != '_')
-            res = (prev - c*x)/ x + str[str.size() - 1] * hashtable[x - 1][str.size() - 1];
+        if (prev != -1 && c == '_'){
+            res = (prev * x + str[str.size() - 1] * x);
+            //cout <<str.size()<<"  "<< res << " childhash \n";
+        }
+        if (prev != -1 && c != '_'){
+            res = ((prev  - c * hashtable[k - 1][str.size() - 1])*x + x*str[str.size() - 1]); 
+            //cout <<str.size()<<"  "<< res <<" c*x "<<x<<" window hash \n";
+        }
         return res;
     }
 
@@ -60,28 +65,30 @@ public:
         if (prev.size() == 0){
             prev = defvect<int>(-1,k);
         }
-        for (int i = 0;  i < k; i++){
+        //cout <<k << " k\n";
+        for (int i = 1;  i < k; i++){
             int kmer_hash = hashfunc(i, kmer,prev[i],c);
             tmp[i] = kmer_hash;
-            if (!list[(m + kmer_hash) % m]){
+            if (!list[ abs(kmer_hash % m)]){
                 result = true;
             }
         }
         if (result){
             for (const auto &i : tmp){
-                list[(m + i) % m] = true;
+                list[abs(i % m)] = true;
             }
         }
         return pair<bool, vector<int>>(result,tmp);
     }
 }; 
 
-vector<int> differentkmer(MAXKMER - MINKMER);
+vector<unsigned int> differentkmer;
 pair<bool, vector<int>> exist (vector<bloom_filter> bf, string kmer, int kstart, string line){
     pair<bool,vector <int>> kmerhashes;
-    if  (kmer.size() >= 20)
+    if  (kmer.size() <= 20)
         kmerhashes = bf[kmer.size() - kstart -1].exists(kmer);
     else
+        //cout << " recurs \n";
         kmerhashes = bf[kmer.size() - kstart -1].exists(kmer, exist(bf, kmer.substr(0,kmer.size()-1), kstart, line).second);
     if (kmerhashes.first)
         differentkmer[kmer.size() - kstart -1] ++;
@@ -97,8 +104,10 @@ pair<bool, vector<int>> exist (vector<bloom_filter> bf, string kmer, int kstart,
 
 int main(){
     vector<bloom_filter> bf;
+    differentkmer.reserve(MAXKMER - MINKMER + 1); 
     bf.reserve(MAXKMER - MINKMER + 1);
     for ( int i = 0; i < MAXKMER - MINKMER + 1; i++){
+        differentkmer.push_back(0);
         bf.push_back(bloom_filter(5000, 0.01));
     }
     ifstream input("new.fastq");
@@ -106,14 +115,13 @@ int main(){
     while (getline(input, line)){
         if (line[0] == '@'){
             getline(input, line);
-            line = line.substr(0, line.size()-1);
             exist(bf, line.substr(0, MAXKMER),MINKMER-1,line);
         }
     }
     input.close();
     ofstream output ("output");
-    for (auto &i : differentkmer){
-        cout << "different " << i+20<<"mers = " << differentkmer[i] <<"\n";
+    for (int i = 0; i <= MINKMER; i++){
+        output<< "different " << i+20<<"mers = " << differentkmer[i] <<"\n";
     }
     return 0;
 }
