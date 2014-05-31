a35ec4ec35fb7cc1aa4b4b1c0beab27d21e0549e
serg <serg@unnormal.(none)>
2014-05-31 19:44:34 +0400

    .

--- Executer/__init__.py
+++ Executer/__init__.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 __author__ = 'serg'
@@ -7,17 +8,18 @@ from run import *
 prefix = "../reads/"
 kmers = [21, 23, 25, 27, 29, 31, 35, 37, 39]
 # kmers = [23]
-reads = [prefix + "frag_2.fastq"]
+_reads = ["frag_2.fastq"]
 assemblers = ["Abyss"]
-for read in reads:
+i = 0
+for read in _reads:
+    i += 1
     for k in kmers:
         for assembler in assemblers:
-            args = [assembler, "--o", "test" + assembler + "/" + str(k), "--k", str(k),
-                    "--i", read, "--f", "fastq", "--r", "long"]
+            if not os.path.exists("test" + assembler + "/" + str(i)):
+                os.mkdir("test" + assembler + "/" + str(i) + "/")
+            args = [assembler, "--o", "test" + assembler + "/" + str(i) + "/" + str(k), "--k", str(k),
+                    "--i", prefix+read, "--f", "fastq", "--r", "long"]
             run(args)
-    # subprocess.call("./../KmerCounting/1", read)
-    # subprocess.call("./../KmerCounting/2", read)
-    # subprocess.call("./../KmerCounting/3", read)
 
 # assemblers = ["SPAdes"]
 # args = [assembler, "--o", "test" + assembler + "/" + str(k), "--k", str(k),
